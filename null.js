3eac91721986e13ff093939c74d2038073f634c6
kiprutop <155768628+kiprutop@users.noreply.github.com>
2024-07-15 22:15:05 +0300

    Add files via upload

---
+++ null.js
@@ -0,0 +1,23 @@
+let triangle = "#";
+let counter = 0;
+if ((counter = 1)) {
+  console.log("#");
+  if ((counter = 2)) {
+    console.log("##");
+  }
+  if ((counter = 3)) {
+    console.log("###");
+  }
+  if ((counter = 4)) {
+    console.log("####");
+  }
+  if ((counter = 5)) {
+    console.log("#####");
+  }
+  if ((counter = 6)) {
+    console.log("######");
+  }
+  if ((counter = 7)) {
+    console.log("#######");
+  }
+}
